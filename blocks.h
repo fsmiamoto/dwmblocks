12fe1cdc0a650f628558ed6a049f2bf766c75c4d
Francisco Miamoto <fsmiamoto@gmail.com>
2020-04-06 21:31:25 -0300

    use const for variables

--- blocks.h
+++ blocks.h
@@ -1,18 +1,15 @@
 typedef struct {
-  char *icon;
-  char *command;
-  unsigned int interval;
-  unsigned int signal;
+  const char *icon;
+  const char *command;
+  const unsigned int interval; // in seconds
+  const unsigned int signal;
 } Block;
 
 static const Block blocks[] = {
-    /*Icon*/ /*Command*/ /*Update Interval in secs*/ /*Update Signal*/
-    {"",     "volume",                0, 10},
-    {"mem ", "mem-perc",              5, 0},
-    {"cpu ", "cpu-perc",              5, 0},
-    {"",    "date +'%H:%M - %m/%d'", 5, 0},
-    /* {"📦 ",  "pacman-update",         3600, 9}, */
-    /* {"🌡 ",   "sensors | awk '/^temp1:/{print $2}'", 5,   0}, */
+    {"", "volume", 0, 10},
+    {"mem ", "mem-perc", 5, 0},
+    {"cpu ", "cpu-perc", 5, 0},
+    {"", "date +'%H:%M - %m/%d'", 5, 0},
 };
 
-static char* delim = " | ";
+static const char *delim = " | ";
