1d9d55f26919aa0b1384bce4f793ab46126dbeb3
Francisco Miamoto <fsmiamoto@gmail.com>
2021-02-06 00:27:50 -0300

    dont use icons

--- blocks.h
+++ blocks.h
@@ -6,13 +6,11 @@ typedef struct {
 } Block;
 
 static const Block blocks[] = {
-    {"", "volume", 0, 10},
-    {" ", "xbacklight -get | awk '{print $1 \"%\"}' ", 0, 11},
-    {"", "battery", 5, 0},
-    {" ", "mem-perc", 5, 0},
-    {" ", "cpu-perc", 5, 0},
-    {" ", "df | grep '/$' | awk '{print $5}'", 5, 0},
-    {" ", "date +'%H:%M - %m/%d'", 5, 0},
+    {"vol ", "volume", 0, 10},
+    {"mem ", "mem-perc", 5, 0},
+    {"cpu ", "cpu-perc", 5, 0},
+    {"home ", "df | grep '/home$' | awk '{print $5}'", 5, 0},
+    {"", "date +'%H:%M - %m月%d日'", 5, 0},
 };
 
 static const char *delim = " | ";
